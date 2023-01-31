9d2b07850f13f1b1f2f4b751943cffa5db275916
naphattar <naphatchalermwai@gmail.com>
2023-01-31 20:11:51 +0700

    add matrix mul

--- DivideandConquer/ex01e4.cpp
+++ DivideandConquer/ex01e4.cpp
@@ -1,4 +1,20 @@
 #include<iostream>
 using namespace std;
 int x,n,k;
-long long arr[]
+long long solve(int x,int n){
+    if(n == 1){
+        return x%k;
+    }
+    if(n%2 == 1){
+        long long md = solve(x,n/2);
+        return (x*md*md)%k;
+    }
+    long long md = solve(x,n/2);
+    return (md*md)%k;
+}
+
+int main(){
+    cin >> x >> n >> k;
+    cout << solve(x,n);
+    return 0;
+}
